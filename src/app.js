50763b8b0483c2da2d4787893e1ca59065adb2f8
Abhishek M <abhi.24.2002@gmail.com>
2024-10-27 02:39:32 +0530

    Add existing project files to Git

---
+++ src/app.js
@@ -0,0 +1,100 @@
+import * as THREE from 'three'
+import { addPass, useCamera, useGui, useRenderSize, useScene, useTick } from './render/init.js'
+// import postprocessing passes
+import { SavePass } from 'three/examples/jsm/postprocessing/SavePass.js'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
+import { BlendShader } from 'three/examples/jsm/shaders/BlendShader.js'
+import { CopyShader } from 'three/examples/jsm/shaders/CopyShader.js'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
+
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
+
+import vertexPars from './shaders/vertex_pars.glsl'
+import vertexMain from './shaders/vertex_main.glsl'
+import fragmentPars from './shaders/fragment_pars.glsl'
+import fragmentMain from './shaders/fragment_main.glsl'
+
+const startApp = () => {
+  const scene = useScene()
+  const camera = useCamera();
+  camera.fov = 95; 
+  camera.updateProjectionMatrix();
+  const gui = useGui()
+  const { width, height } = useRenderSize()
+
+  // settings
+  const MOTION_BLUR_AMOUNT = 0.725
+
+  // lighting
+  const dirLight = new THREE.DirectionalLight('#2596be', 0.3)
+  dirLight.position.set(2, 2, 2)
+
+  const ambientLight = new THREE.AmbientLight('#2596be', 0.2)
+  scene.add(dirLight, ambientLight)
+
+
+  // meshes
+  const geometry = new THREE.IcosahedronGeometry(1, 400)
+  const material = new THREE.MeshStandardMaterial({
+
+    onBeforeCompile: (shader) => {
+      // storing a reference to the shader object
+      material.userData.shader = shader
+
+      // uniforms
+      shader.uniforms.uTime = { value: 0 }
+
+      const parsVertexString = /* glsl */ `#include <displacementmap_pars_vertex>`
+      shader.vertexShader = shader.vertexShader.replace(
+        parsVertexString,
+        parsVertexString + vertexPars
+      )
+
+      const mainVertexString = /* glsl */ `#include <displacementmap_vertex>`
+      shader.vertexShader = shader.vertexShader.replace(
+        mainVertexString,
+        mainVertexString + vertexMain
+      )
+
+      const mainFragmentString = /* glsl */ `#include <normal_fragment_maps>`
+      const parsFragmentString = /* glsl */ `#include <bumpmap_pars_fragment>`
+      shader.fragmentShader = shader.fragmentShader.replace(
+        parsFragmentString,
+        parsFragmentString + fragmentPars
+      )
+      shader.fragmentShader = shader.fragmentShader.replace(
+        mainFragmentString,
+        mainFragmentString + fragmentMain
+      )
+    },
+  })
+
+  const ico = new THREE.Mesh(geometry, material)
+  scene.add(ico)
+
+  // GUI
+  const cameraFolder = gui.addFolder('Camera');
+  cameraFolder.add(camera, 'fov', 1, 120).onChange(() => {
+      camera.updateProjectionMatrix(); // Update projection matrix whenever FOV changes
+  });
+  cameraFolder.open();
+
+  // // postprocessing
+  // const renderTargetParameters = {
+  //   minFilter: THREE.LinearFilter,
+  //   magFilter: THREE.LinearFilter,
+  //   stencilBuffer: false,
+  // }
+
+
+  // postprocessing
+  // addPass(new UnrealBloomPass(new THREE.Vector2(width, height), 0.7, 0.4, 0.4))
+
+  useTick(({ timestamp, timeDiff }) => {
+    const time = timestamp / 5000
+    material.userData.shader.uniforms.uTime.value = time
+  })
+}
+
+export default startApp
